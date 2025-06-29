a628a258a31c55c3c0c60b201397ea8e0546e9ae
Jorge Rios <jorgeandraderios7@gmail.com>
2025-06-29 18:52:26 -0300

    HorizontalCalendarConfiguration2

--- app/navigation/HorizontalCalendar.tsx
+++ app/navigation/HorizontalCalendar.tsx
@@ -12,15 +12,20 @@ import {
 } from 'react-native';
 
 interface Props {
-  onDateChange: (date: Date) => void;
+    onDateChange: (date: Date) => void;
 }
 
-const ITEM_WIDTH = 90;
-const ITEM_SPACING = 10;
+const ITEM_WIDTH = 70;
+const ITEM_SPACING = 0;
+const SPACING_DEFAULT = 10;
+const DEFAULT_SIZE = 80;
+const RADIUS_DEFAULT = 16;
 const SCREEN_WIDTH = Dimensions.get('window').width;
-const CENTER_OFFSET = (SCREEN_WIDTH - ITEM_WIDTH) / 3;
+const CENTER_OFFSET = (SCREEN_WIDTH - ITEM_WIDTH) / 4;
 
-const HorizontalCalendar: React.FC<Props> = ({onDateChange}) => {
+
+
+const HorizontalCalendar: React.FC<Props> = ({ onDateChange }) => {
     const scrollRef = useRef<ScrollView>(null);
     const [currentIndex, setCurrentIndex] = useState(0);
     const scrollX = useRef(new Animated.Value(0)).current;
@@ -41,12 +46,12 @@ const HorizontalCalendar: React.FC<Props> = ({onDateChange}) => {
     }, []);
 
     const scrollToIndex = (index: number, isInit: boolean) => {
-    if (index < 0 || index >= months.length && !isInit) return;
-    const x = index * (ITEM_WIDTH + ITEM_SPACING);
-    scrollRef.current?.scrollTo({ x, animated: true });
-    setCurrentIndex(index);
-    onDateChange(new Date(currentYear, index, 1));
-};
+        if (index < 0 || index >= months.length && !isInit) return;
+        const x = index * (ITEM_WIDTH + ITEM_SPACING);
+        scrollRef.current?.scrollTo({ x, animated: true });
+        setCurrentIndex(index);
+        onDateChange(new Date(currentYear, index, 1));
+    };
 
     const handleNext = () => {
         console.log('button Next pressed!');
@@ -68,54 +73,61 @@ const HorizontalCalendar: React.FC<Props> = ({onDateChange}) => {
         <View style={styles.container}>
             <Text style={styles.title}>{currentYear}</Text>
 
-            <View style={styles.row}>
+            <View style={styles.calendarContent}>
+
                 <TouchableOpacity style={styles.button} onPress={handlePrevious}>
                     <Feather name="chevron-left" size={24} color="black" />
                 </TouchableOpacity>
 
-                <Animated.ScrollView
-                    ref={scrollRef}
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
-                    snapToInterval={ITEM_WIDTH + ITEM_SPACING}
-                    decelerationRate="fast"
-                    contentContainerStyle={{ paddingHorizontal: CENTER_OFFSET }}
-                    onMomentumScrollEnd={onScrollEnd}
-                    onScroll={Animated.event(
-                        [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-                        { useNativeDriver: false }
-                    )}
-                    scrollEventThrottle={16}
-                >
-                    {months.map((month, index) => (
-                        <Animated.View
-                            key={index}
-                            style={[
-                                styles.card,
-                                index === currentIndex && styles.activeCard,
-                                {
-                                    transform: [
-                                        {
-                                            scale: scrollX.interpolate({
-                                                inputRange: [
-                                                    (index - 1) * (ITEM_WIDTH + ITEM_SPACING),
-                                                    index * (ITEM_WIDTH + ITEM_SPACING),
-                                                    (index + 1) * (ITEM_WIDTH + ITEM_SPACING),
-                                                ],
-                                                outputRange: [0.9, 1, 0.9],
-                                                extrapolate: 'clamp',
-                                            }),
-                                        },
-                                    ],
-                                },
-                            ]}
-                        >
-                            <Text style={[styles.monthText, index === currentIndex && styles.activeText]}>
-                                {month}
-                            </Text>
-                        </Animated.View>
-                    ))}
-                </Animated.ScrollView>
+                <View style={styles.row}>
+
+
+                    <Animated.ScrollView
+                        ref={scrollRef}
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        snapToInterval={ITEM_WIDTH + ITEM_SPACING}
+                        decelerationRate="fast"
+                        contentContainerStyle={{ paddingHorizontal: CENTER_OFFSET }}
+                        onMomentumScrollEnd={onScrollEnd}
+                        onScroll={Animated.event(
+                            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+                            { useNativeDriver: false }
+                        )}
+                        scrollEventThrottle={16}
+                    >
+                        {months.map((month, index) => (
+                            <Animated.View
+                                key={index}
+                                style={[
+                                    styles.card,
+                                    index === currentIndex && styles.activeCard,
+                                    {
+                                        transform: [
+                                            {
+                                                scale: scrollX.interpolate({
+                                                    inputRange: [
+                                                        (index - 1) * (ITEM_WIDTH + ITEM_SPACING),
+                                                        index * (ITEM_WIDTH + ITEM_SPACING),
+                                                        (index + 1) * (ITEM_WIDTH + ITEM_SPACING),
+                                                    ],
+                                                    outputRange: [0.9, 1, 0.9],
+                                                    extrapolate: 'clamp',
+                                                }),
+                                            },
+                                        ],
+                                    },
+                                ]}
+                            >
+                                <Text style={[styles.monthText, index === currentIndex && styles.activeText]}>
+                                    {month}
+                                </Text>
+                            </Animated.View>
+                        ))}
+                    </Animated.ScrollView>
+
+
+                </View>
 
                 <TouchableOpacity style={styles.button} onPress={handleNext}>
                     <Feather name="chevron-right" size={24} color="black" />
@@ -126,34 +138,43 @@ const HorizontalCalendar: React.FC<Props> = ({onDateChange}) => {
 };
 
 const styles = StyleSheet.create({
-    container: { padding: 10, borderBottomColor: 'black', borderBottomWidth: 1, backgroundColor: Colors.light.background },
-    title: { textAlign: 'center', fontSize: 20, marginBottom: 20 },
-    row: {
+    container: { padding: SPACING_DEFAULT, borderBottomColor: 'black', borderBottomWidth: 1, backgroundColor: Colors.light.background },
+    title: { textAlign: 'center', fontWeight: 'bold', fontSize: 16, marginBottom: SPACING_DEFAULT, backgroundColor: Colors.light.background },
+
+    calendarContent: {
         flexDirection: 'row',
+        backgroundColor: Colors.light.background,
+        justifyContent: 'center',
         alignItems: 'center',
     },
+
+    row: {
+        width: '59%',
+        height: DEFAULT_SIZE,
+        backgroundColor: Colors.light.background,
+    },
     button: {
-        width: 60,
-        height: 60,
+        width: DEFAULT_SIZE,
+        height: DEFAULT_SIZE,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: Colors.light.background,
-        borderRadius: 30,
+        borderRadius: RADIUS_DEFAULT
     },
     card: {
         width: ITEM_WIDTH,
-        height: 80,
+        height: DEFAULT_SIZE,
         marginHorizontal: ITEM_SPACING / 2,
         backgroundColor: Colors.light.background,
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 12,
-        padding: 10,
+        borderRadius: RADIUS_DEFAULT,
     },
     activeCard: {
-        backgroundColor: Colors.light.highlightBackgroun_1,
+        backgroundColor: Colors.light.highlightBackgroun_2,
         borderColor: Colors.light.shadow,
         borderWidth: 1,
+
     },
     monthText: {
         fontSize: 16,
